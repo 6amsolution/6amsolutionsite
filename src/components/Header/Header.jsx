c477ffd6d05e4479848c7763857d0ad15650bb8a
6amsolution <6amsolution@gmail.com>
2024-01-31 17:43:32 +0500

    Initial commit

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,263 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+      <div className='w-full h-screen bg-slate-950 text-white'>
+      
+<div className='w-full h-20vh flex'>
+    <div className='w-20 flex flex-wrap h-auto ml-40'>
+    <img src='images/logo.png' width="60px" height="40px"/>
+    </div>
+
+    <div className='w-80 h-20 flex flex-wrap justify-center items-center ml-80'>
+        <ul className='flex font-bolder text-md lg:flex-row lg:space-x-8 lg:mt-8'>
+            <li className='text-sky-500'>Home</li>
+            <li>Services</li>
+            <li>Products</li>
+            <li>Protfolio</li>
+            <li>Team</li>
+            <li>Course</li>
+            <li>Contact</li>
+        </ul>
+    </div>
+
+<div className='w-20 h-20 mx-auto mr-20 mt-10'>
+    <img src="images/menu3.png" width="25px" height="20px"/>
+</div>
+</div>
+
+<div className='w-1/2 h-40vh  p-6 text-center justify-center mt-12 ml-80'>
+ <h1 className='text-4xl font-extrabold whitespace-break-space'>We Develop <span className='text-sky-400'>Websites and Digital<br /> Preferences.</span> that bing your online<br /> 
+    business to success</h1>
+</div>
+
+<div className='w-1/2 h-20vh bg-slate-900 p-10 text-center justify-center ml-80 rounded-lg'>
+    <h4 className='text-sd'>At 6AMSolution , we combine the latest technologies, technical<br /> 
+    skills and industry experts to build the most Powerful Websites
+     and <br /> Grow your business Online that help you to scale up your business.</h4>
+</div>
+
+<div className='w-1/2 h-10vh flex text-black ml-80 mt-10'>
+    <button className='border-none px-4 py-1 text-white rounded-full mr-12 ml-60 bg-sky-600 font-semibold'>Our Services</button>
+    <button className='border-2 px-4 py-1 text-white rounded-full'>Get Appointment</button>
+</div>
+</div>
+
+<div className='w-full h-20 bg-slate-950 flex flex-wrap justify-start items-center gap-1 mt-0 '>
+    <h1 className=' w-full h-10 text-center mt-10 text-2xl text-white'><span className='text-orange-600 font-semibold'>Technologies</span> We Use</h1>
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-android-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-ios-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-php-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-firebase-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-google-maps-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-laravel-icon.svg" width="170" height="70"/>
+</div>
+
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-flutter-icon.svg" width="170" height="70"/>
+</div>
+<div className='w-50 h-20'>
+<img src="https://6amtech.com/wp-content/uploads/2023/12/6amTech-mysql-icon.svg" width="170" height="70"/>
+</div>
+
+</div>
+
+
+
+
+<div className='w-full h-60vh mt-10 bg-slate-950 pt-20'>
+<div className='w-3/4 h-10vh m-auto pt-6'>
+<h1 className='text-4xl text-center py-4 font-semibold text-white'>Explore Our <span className='text-orange-600'>Services
+</span> for You</h1>
+<p className='text-center py-2 text-xl text-white'>We provide different services to kick-start your business and help to <br />
+transform your idea into the real world.</p>
+</div>
+
+<div className='w-full h-100vh bg-slate-950 flex flex-wrap justify-center gap-10 m-auto mt-20 pt-20'>
+    <div className='w-60 h-100vh bg-white'>
+        <img src='https://6amtech.com/wp-content/uploads/2023/12/6amtech-installation-services.webp' width="250px" height="100px"/>
+        <h1 className='text-center pt-4 text-2xl font-extrabolder'>Installation</h1>
+        <p className='text-center py-2 text-gray-500'>Start your business right<br/> away with our installation services.</p>
+        <button className='ml-8 mt-1 border-2 border-orange-400 px-4 py-2 rounded-full
+         text-orange-500 font-semibold'>Installation Plans</button>
+    </div>
+
+    <div className='w-60 h-100vh bg-white pb-4'>
+
+        <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-product-customization-services.webp"/>
+        <h1 className='text-center pt-4 text-2xl font-extrabolder'>Customization</h1>
+        <p className='text-center py-2 text-gray-500'>Get our product customized <br/>to your new feature <br/>requirements.</p>
+        <button className='ml-16 mt-1 border-2 border-orange-400 px-4 py-2 rounded-full
+         text-orange-500 font-semibold'>Learn More</button>
+
+    </div>
+
+    
+<div className='w-60 h-100vh bg-white pb-4'>
+
+        <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-development-services.webp"/>
+        <h1 className='text-center pt-4 text-2xl font-extrabolder'>Development</h1>
+        <p className='text-center py-2 text-gray-500'>Bring your idea to reality<br/> with our expert<br/> development services.</p>
+        <button className='ml-16 mt-1 border-2 border-orange-400 px-4 py-2 rounded-full
+         text-orange-500 font-semibold'>Learn More</button>
+
+    </div>
+</div>
+</div>
+
+<div className='w-full h-60 p-6 bg-slate-950'>
+
+    <div className='w-2/4 h-60 bg-white m-auto p-6 rounded-md'>
+    <h1 className='text-center text-black text-2xl py-4'>Get <span
+     className='font-semibold text-orange-500'>Resources & Guidance</span></h1>
+
+    <p className='text-gray-500 text-center'>Dive into our latest blogs & articles, informative product<br /> 
+    documentation, and video tutorials to understand our products better.</p>
+    </div>
+
+ <div className='w-3/4 h-86 ml-48 flex justify-evenly items-start gap-4 bg-slate-950'>
+
+<div className='w-1/4 h-20 bg-slate-950'>
+    <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-company-news.webp"/>
+    <h1 className='text-center py-2 text-2xl font-medium text-sky-700'>Company News</h1>
+    <p className='text-center p-2 text-gray-400'>See how we’re doing as a <br/>
+    company and keep our bonds in<br />
+     a tight knot as a tech family.</p>
+    <button className='text-center ml-20 mt-2 border-2 border-orange-500 py-2 px-4
+     rounded-full text-orange-500 font-medium'>Read News</button>
+
+</div>
+
+<div className='w-1/4 h-20 rounded-xl'>
+    <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-blogs-and-articles.webp"/>
+    <h1 className='text-center py-2 text-2xl font-medium text-sky-700'>Blogs & Articles</h1>
+    <p className='text-center p-2 text-gray-400'>
+    Get all the freshly brewed <br />
+    content from 6amTech’s<br />
+    center for in-depth learning.</p>
+
+    <button className='text-center ml-20 mt-2 border-2 border-orange-500 py-2 px-4
+     rounded-full text-orange-500 font-medium'>View Blogs</button>
+
+</div>
+
+<div className='w-1/4 h-20'>
+    <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-documentation-and-user-manual.webp"/>
+    <h1 className='text-center py-2 text-2xl font-medium text-sky-700'>Documentations</h1>
+    <p className='text-center p-2 text-gray-400'>
+    Learn how to use 6amTech’s<br /> 
+    solutions with detailed technical<br />
+     documentation & user manual.
+</p>
+    <button className='text-center ml-20 mt-2 border-2 border-orange-500 py-2 px-4
+     rounded-full text-orange-500 font-medium'>Download Docs</button>
+
+</div>
+
+<div className='w-1/4 h-20 bg-slate-950'>
+    <img src="https://6amtech.com/wp-content/uploads/2023/12/6amtech-watch-video.webp"/>
+    <h1 className='text-center py-2 text-2xl font-medium text-sky-700'>Watch Video</h1>
+    <p className='text-center p-2 text-gray-400'>
+    Get a hands-on experience with<br/>
+     our eCommerce solutions with <br />
+     proper visualization.
+     </p>
+     <button className='text-center ml-20 mt-2 border-2 border-orange-500 py-2 px-4
+      rounded-full text-orange-500 font-medium'>Watch Video</button>
+</div>
+ </div>
+</div>
+
+<div className='w-full mt-96 h-96 bg-slate-950'>
+    <div className='w-3/4 h-96 bg-pink-50 ml-60 rounded-lg pt-10'>
+        <div className='w-3/4 h-24 m-auto p-2'>
+    <h1 className='text-center text-2xl py-2 ml-10'>Join The <span className='font-semibold
+     text-orange-500'>‘Inner Circle’</span></h1>
+
+     <p className='text-gray-600 py-2 text-center'>Subscribe to our newsletter and be the first to get all the latest insights!</p>
+        </div>
+
+        <div className='w-3/4 h-48  m-auto mt-10'>
+
+        <input className='w-3/4 p-2 border-pink-100 border-2 bg-pink-100 rounded-xl ml-20 outline-none' 
+        type="text" placeholder='Your name'/>
+
+<input className='w-3/4 p-2 border-pink-100 border-2 bg-pink-100 rounded-xl mt-4 ml-20 outline-none text-black' 
+        type="email" placeholder='Your email'/>
+
+        <button className='py-2 px-10 border-2 border-orange-500 text-center 
+        float-right mt-6 p-2 text-white bg-orange-500 rounded-2xl mr-32 font-bold'>Subscribe</button>
+        </div>
+    
+    </div>
+</div>
+
+<div className='w-full h-96 bg-slate-950 flex gap-10'>
+    <div className='w-80 h-100% ml-20'>
+        <div className='w-40 flex gap-2'>
+   <img src="images/logo.png" width="40px" height="0px" className='mt-6 rounded-2xl bg-white text-center'/>
+   <h1 className='text-white mt-10 font-extrabold mr-1'>6amSolution</h1>
+    </div>
+<div className='text-white w-full my-8'> 
+   <p className='text-start text-sm'><span className='font-extrabold text-orange-500'>6amSolution</span> helps businesses to build and grow with 
+   versatile eCommerce software solutions and services.
+</p>
+</div>
+<div className='w-full flex gap-6'>
+
+<img src="https://i.pinimg.com/564x/65/f1/31/65f131b166a11b914d56cb22bb8c7cf9.jpg"
+ width="30px"className='rounded-full overflow-hidden'/>
+
+<img src="https://i.pinimg.com/564x/29/6b/a0/296ba00c8098dee633d2eab414cea364.jpg"
+ width="30px"className='rounded-full overflow-hidden'/>
+
+<img src="https://toppng.com/uploads/preview/twitter-x-new-logo-round-icon-png-11692480241tdbz6jparr.webp"
+ width="30px"className='rounded-full overflow-hidden'/>
+ 
+ <img src="https://img.lovepik.com/png/20231019/youtube-icon-youtube-logo-black-and-icon-youtube-play-button_266811_wh1200.png"
+ width="30px"className='rounded-full overflow-hidden'/>
+ 
+</div>
+
+<div className='w-full flex gap-6 mt-10'>
+
+<img src="https://6amtech.com/wp-content/uploads/2023/03/6amTech-codecanyon-logo.svg" width="130px" height="100px"/>
+
+<button className='py-1 px-1 border-2 border-white rounded-2xl text-md bg-white'>Support Ticket</button>
+</div>
+    </div>
+
+    <div className='w-72 h-100% bg-green-700'>
+        <h1>This is a Book</h1>
+    </div>
+
+    <div className='w-72 h-100% bg-red-900'>
+        <h1>This is a Book</h1>
+    </div>
+    <div className='w-72 h-100% bg-orange-900'>
+        <h1>This is a Book</h1>
+    </div>
+</div>
+ </>
+  )
+}
+
+
+export default Header;
